c60fef201d3e3c55bf8e648ddd1353d94fcddf33
Brandan Sayarath <bsayarath@gmail.com>
2020-02-07 13:01:07 -0800

    passed test that determined if object was instantiated

--- __tests__/super-galactic-age-calculator.test.js
+++ __tests__/super-galactic-age-calculator.test.js
@@ -1,8 +1,14 @@
 import { ageCalc } from ".././src/super-galactic-age-calculator.js"; 
 
-describe("ageCalc(age)", () => { 
+describe('ageCalc', () => {
+  let userAge;
+  beforeEach(() => {
+    userAge = new ageCalc(20);
+  });
 
-  test("should determine if ageCalc object is instantiated correctly", () => {
-    expect.ageCalc(30).toBe(30);
+
+
+  test("should determine if ageCalc object is instantiated correctly with user inputted age", () => {
+    expect(userAge.age).toBe(20);
   })
 })
