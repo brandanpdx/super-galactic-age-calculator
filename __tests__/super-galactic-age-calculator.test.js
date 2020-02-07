95f8dc9c73e49f855add322a3200c700c86b7fdb
Brandan Sayarath <bsayarath@gmail.com>
2020-02-07 13:31:04 -0800

    add test to determine age on Mercury and fail as expected

--- __tests__/super-galactic-age-calculator.test.js
+++ __tests__/super-galactic-age-calculator.test.js
@@ -9,6 +9,11 @@ describe('ageCalc', () => {
 
 
   test("should determine if ageCalc object is instantiated correctly with user inputted age", () => {
-    expect(userAge.age).toBe(20);
+    expect(userAge.age).toEqual(20);
+  })
+
+  test("should determine age on Mercury if age on Earth is 20 years old", () => {
+    let mercuryAge = (userAge * mercury);
+    expect(mercuryAge).toEqual(83.3);
   })
 })
