79cbe18779e3758209efaa0b8e09474a4d442647
Brandan Sayarath <bsayarath@gmail.com>
2020-02-07 14:21:25 -0800

    passed test that determined age on Mercury

--- __tests__/super-galactic-age-calculator.test.js
+++ __tests__/super-galactic-age-calculator.test.js
@@ -2,18 +2,19 @@ import { ageCalc } from ".././src/super-galactic-age-calculator.js";
 
 describe('ageCalc', () => {
   let userAge;
+
   beforeEach(() => {
     userAge = new ageCalc(20);
   });
-
-
 
   test("should determine if ageCalc object is instantiated correctly with user inputted age", () => {
     expect(userAge.age).toEqual(20);
   })
 
-  test("should determine age on Mercury if age on Earth is 20 years old", () => {
-    let mercuryAge = (userAge * mercury);
-    expect(mercuryAge).toEqual(83.3);
+  test("should determine age on Mercury when age on Earth is 20 years old", () => {
+    let mercuryAge = parseFloat((userAge.age / userAge.mercury).toFixed(2));
+    expect(mercuryAge).toEqual(83.33);
   })
 })
+
+// parseFloat((userAge.age / planets.mercury).toFixed(2));
--- src/super-galactic-age-calculator.js
+++ src/super-galactic-age-calculator.js
@@ -1,5 +1,15 @@
 export class ageCalc {
   constructor(age) {
     this.age = age;
+    this.mercury = 0.24;
   }
+
 }
+
+
+
+// let mercury = 0.24;
+// let venus = 0.62;
+// let mars = 1.88;
+// let jupiter = 11.86;
+
