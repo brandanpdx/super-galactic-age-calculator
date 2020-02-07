f1935d8e3ac511ea9a1a5d9f69002ea8da06dc1f
Brandan Sayarath <bsayarath@gmail.com>
2020-02-07 14:47:01 -0800

    pass test that determins age on Mars

--- src/super-galactic-age-calculator.js
+++ src/super-galactic-age-calculator.js
@@ -3,6 +3,7 @@ export class ageCalc {
     this.age = age;
     this.mercury = 0.24;
     this.venus = 0.62;
+    this.mars = 1.88;
 
 
   }
@@ -11,8 +12,7 @@ export class ageCalc {
 
 
 
-// let mercury = 0.24;
-// let venus = 0.62;
+
 // let mars = 1.88;
 // let jupiter = 11.86;
 
